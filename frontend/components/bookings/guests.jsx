85b132ea65d9fce2b0b0e3f0bb4568866c245ff6
Jeffrey Fan <jeffreyfan@Jeffreys-MacBook-Air.local>
2017-01-20 15:13:34 -0800

    create production readme

--- frontend/components/bookings/guests.jsx
+++ frontend/components/bookings/guests.jsx
@@ -8,6 +8,14 @@ class Guests extends React.Component {
   }
 
   render() {
+    this.props.bookings.sort((a, b) => {
+      if (a.arrive_date < b.arrive_date) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+    
     return(
       <div className="dashboard-guests-container">
         <div className="guests-title">My Guests</div>
