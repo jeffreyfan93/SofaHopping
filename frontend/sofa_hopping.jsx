f300f0c27ce5b96b6ea3d30d569a1ae2469576a4
Jeffrey Fan <jeffreyfan93@gmail.com>
2017-01-20 01:33:50 -0800

    add review seeds and a little styling

--- frontend/sofa_hopping.jsx
+++ frontend/sofa_hopping.jsx
@@ -3,11 +3,6 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-// testing
-import { fetchBookings, createBooking, deleteBooking } from './actions/booking_actions';
-import { addSofa } from './actions/sofa_actions';
-import { fetchReviews, addReview, updateReview, deleteReview } from './actions/review_actions';
-
 document.addEventListener('DOMContentLoaded', () => {
   let store;
   if (window.currentUser) {
@@ -18,16 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   window.store = store;
-  window.fetchReviews = fetchReviews;
-  window.addReview = addReview;
-  window.updateReview = updateReview;
-  window.deleteReview = deleteReview;
-  window.review = {review:{body:"this is a bad sofa", user_id:1, sofa_id:3}};
-  const arrive_date = new Date(2017,2,3);
-  const depart_date = new Date(2017,2,6);
-  window.sofa = { address: "58 Middle Point Rd", city: 'San Francisco', state: 'CA', zip: 94124, num_of_guests: 1, user_id: 1};
-  window.booking = {arrive_date: arrive_date, depart_date: depart_date, num_of_guests: 2, sofa_id: 1, guest_id: 2, host_id: 1};
-  // window.booking1 = {arrive_date: arrive_date, depart_date: depart_date, num_of_guests: 2, sofa_id: 3, guest_id: 1, host_id: 2};
 
   const root = document.getElementById('root');
   ReactDOM.render(<Root store={ store } />, root);
